9bd43ab8f6eb88f8daa57641e8d601f659fbb853
Jumas <kbbyfl91@gmail.com>
2024-02-18 16:14:54 +0300

    Add filetype and regex patterns ignore

--- lua/cosco.lua
+++ lua/cosco.lua
@@ -5,7 +5,9 @@ local module = require("cosco.module")
 ---@field opt string Your config option
 local config = {
     cosco_ignore_comment_lines = vim.g.cosco_ignore_comment_lines or 0,
-    cosco_ignore_ft_pattern = vim.g.cosco_ignore_ft_pattern or {}
+    cosco_ignore_ft_pattern = vim.g.cosco_ignore_ft_pattern or {},
+    cosco_filetype_whitelist = vim.g.cosco_filetype_whitelist or nil,
+    cosco_filetype_blacklist = vim.g.cosco_filetype_blacklist or nil
 }
 
 ---@class MyModule
@@ -21,8 +23,7 @@ M.setup = function(args)
     M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
-M.comma_or_semi_colon = function()
-    return module.comma_or_semi_colon(M.config)
-end
+M.comma_or_semi_colon =
+    function() return module.comma_or_semi_colon(M.config) end
 
 return M
