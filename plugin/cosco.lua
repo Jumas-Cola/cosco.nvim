77d9261003728a5c9f04ab50ba6a88e3f2b7d387
Jumas <kbbyfl91@gmail.com>
2024-02-18 23:12:14 +0300

    Upd

--- plugin/cosco.lua
+++ plugin/cosco.lua
@@ -1,1 +1,47 @@
-vim.api.nvim_create_user_command("CommaOrSemiColon", require("cosco").comma_or_semi_colon, {})
+-- File: plugin/cosco.lua
+
+-- ===============================================
+-- Examples on how to use it (also on the README):
+-- ===============================================
+
+-- autocmd FileType c,cpp,css,java,javascript,perl,php,jade nnoremap <silent> <Leader>; <Plug>(cosco-commaOrSemiColon)
+-- autocmd FileType c,cpp,css,java,javascript,perl,php,jade inoremap <silent> <Leader>; <C-o><Plug>(cosco-commaOrSemiColon)
+-- command! CommaOrSemiColon call cosco#commaOrSemiColon()
+
+local cosco = require("cosco")
+
+vim.api.nvim_create_user_command("CommaOrSemiColon", cosco.comma_or_semi_colon, {})
+
+if not vim.g.auto_comma_or_semicolon then
+  vim.g.auto_comma_or_semicolon = 0
+end
+
+if not vim.g.auto_comma_or_semicolon_events then
+  vim.g.auto_comma_or_semicolon_events = { "InsertLeave" }
+end
+
+local function auto_comma_or_semicolon()
+  if vim.g.auto_comma_or_semicolon >= 1 then
+    cosco.comma_or_semi_colon()
+  end
+end
+
+for _, event in ipairs(vim.g.auto_comma_or_semicolon_events) do
+  vim.cmd(string.format("autocmd %s * CommaOrSemiColon", event))
+end
+
+function AutoCommaOrSemiColonToggle()
+  if vim.g.auto_comma_or_semicolon >= 1 then
+    vim.g.auto_comma_or_semicolon = 0
+    print("AutoCommaOrSemiColon is OFF")
+  else
+    vim.g.auto_comma_or_semicolon = 1
+    print("AutoCommaOrSemiColon is ON")
+  end
+end
+
+function CommaOrSemiColon()
+  if vim.g.auto_comma_or_semicolon >= 1 then
+    cosco.comma_or_semi_colon()
+  end
+end
