447d7faab8e5e86f4d65c1f256ddd38973741f0d
Jumas <kbbyfl91@gmail.com>
2024-02-20 11:13:02 +0300

    Add helper tests

--- tests/cosco/cosco_spec.lua
+++ tests/cosco/cosco_spec.lua
@@ -8,6 +8,43 @@ describe("helpers", function()
   it("strip helper func", function()
     assert(plugin.strip(" Hello!  ") == "Hello!", "strip() func works")
   end)
+
+  it("get_next_non_blank_line_num helper func", function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "one", "", "", "", "five" })
+
+    vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+    assert(5 == plugin.get_next_non_blank_line_num(1))
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "one", "", "", "", "" })
+
+    vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+    assert(nil == plugin.get_next_non_blank_line_num(1))
+  end)
+
+  it("get_prev_non_blank_line_num helper func", function()
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "one", "", "", "", "five" })
+
+    vim.api.nvim_win_set_cursor(0, { 5, 0 })
+
+    assert(1 == plugin.get_prev_non_blank_line_num(5))
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, { "", "", "", "", "five" })
+
+    vim.api.nvim_win_set_cursor(0, { 5, 0 })
+
+    assert(nil == plugin.get_prev_non_blank_line_num(5))
+  end)
+end)
+
+it("get_line_by_num helper func", function()
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, { "one", "", "", "", "five" })
+
+  vim.api.nvim_win_set_cursor(0, { 1, 0 })
+
+  assert("one" == plugin.get_line_by_num(1))
+  assert("five" == plugin.get_line_by_num(5))
 end)
 
 describe("Previous line ending with `,`", function()
