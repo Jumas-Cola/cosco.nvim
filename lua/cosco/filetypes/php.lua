4b784e429d6bf699adcaf0c36d99a8aec0dcfce6
Jumas <kbbyfl91@gmail.com>
2024-02-23 21:00:44 +0300

    Update php filetype

--- lua/cosco/filetypes/php.lua
+++ lua/cosco/filetypes/php.lua
@@ -11,6 +11,8 @@ M.parse = function()
   elseif vim.b.prev_line_last_char == "," then
     if vim.b.next_line_first_char == ")" then
       cosco.remove_comma_or_semi_colon()
+    elseif vim.b.next_line_first_char ~= "]" then
+      cosco.make_it_a_comma()
     end
   else
     vim.b.was_extension_executed = false
